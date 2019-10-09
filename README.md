ba5967d3af3ff587e167d9b9cc3a442ab2e98c33
Dinesh257 <kuntamukkala.dinesh@gmail.com>
2019-10-09 16:25:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cellhighlight
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cellhighlight)
---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { CollectionView } from 'wijmo/wijmo';
+
+import * as wjcCore from 'wijmo/wijmo';
+import * as wjcGrid from 'wijmo/wijmo.angular2.grid';
+import { GroupRow, CellRangeEventArgs, } from 'wijmo/wijmo.grid';
+import { FlexGridDetailProvider } from 'wijmo/wijmo.grid.detail';
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    @ViewChild('flex') jsonFlex: wjcGrid.WjFlexGrid;
+    treeData: CollectionView;
+
+ ngOnInit() {
+       this.treeData = new CollectionView(this.getTreeData());
+       this.jsonFlex.refresh();
+    }
+  
+     onCellEditEnding(event: CellRangeEventArgs, gridname: wjcGrid.WjFlexGrid) {
+      let data = Number(gridname.activeEditor.value.replace(/[^0-9\.-]+/g, ''));
+      console.log(1);
+      // alert(gridname.columns.length);
+
+    }
+
+    reload(){
+      this.treeData = new CollectionView([]);
+      this.treeData = new CollectionView(this.getTreeData());
+      this.jsonFlex.refresh();
+
+    }
+
+    getTreeData() {
+        return [
+          { name: 'Theories', length: '2:02' },
+          { name: 'Giant Eyes', length: '3:29' },
+          { name: 'Jovian Moons', length: '1:02' , items:[{ name: 'Jovian Moons', length: '1:02'}]},
+          { name: 'Open Minds', length: '2:41' },
+          { name: 'Spacetronic Eyes', length: '3:41' } 
+        ]
+    }
+}
